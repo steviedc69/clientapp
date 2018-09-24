62d564787d77ddd1c44aa0eb52b8e18ad018dc57
Laurena1997 <laurena.nijs@realdolmen.com>
2018-09-24 16:45:51 +0200

    method added

--- ClientRepository/src/test/java/com/realdolmen/maven/clientrepository/facade/ClientFacadeTest.java
+++ ClientRepository/src/test/java/com/realdolmen/maven/clientrepository/facade/ClientFacadeTest.java
@@ -8,10 +8,28 @@ package com.realdolmen.maven.clientrepository.facade;
 import org.junit.Test;
 import static org.junit.Assert.*;
 
+
+@RunWith(MockitoJUnitRunner.class)
 /**
  *
  * @author SDOAX36
  */
+PersonRepository personRepository;
+
+    @Mock
+    private PersonService personService;
+
+    @Mock
+    private AddressService addressService;
+
+    @Before
+    public void init() {
+        personRepository = new PersonRepository();
+        personService = new PersonService(personRepository);
+        addressService = new AddressService();
+    }
+
+
 public class ClientFacadeTest {
     
     public ClientFacadeTest() {
@@ -29,6 +47,40 @@ public class ClientFacadeTest {
      */
     @Test
     public void testFindClientById() {
+    }
+    
+    
+    @Test
+    public void testAddAddressForPersonSuccess() throws NoQueryPossibleException {
+        //init mock data 
+        
+        when(addressService.addAddress()).thenReturn("address");
+
+        //do the test
+        boolean result = ClientFacade.addAddressforPerson(id, address);
+
+        //verify your result
+        assertTrue(result);
+        verify(personService, times(1)).findById(0);
+        verify(addressService times(1)).AddAddress();
+
+    }
+
+    @Test
+    public void testAddAddressForFirmSuccess() {
+        //init data
+
+        //do the test
+        //verify your result
+    }
+
+    @Test
+    public void AddAddressThrowsException() throws NoPersonFoundException {
+        //init data 
+        when(personService.findById()).thenThrow(NoPersonFoundException);
+
+        //test
+        //verify
     }
     
 }
