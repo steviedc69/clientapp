9a1ec540c93cc3d565540a091697e302ef6b6e91
L3RY5 <lerys91@hotmail.com>
2018-09-24 15:19:16 +0200

    initial commit pof our maven Git project

--- ClientRepository/src/main/java/com/realdolmen/maven/clientrepository/repositories/AddressRepository.java
+++ ClientRepository/src/main/java/com/realdolmen/maven/clientrepository/repositories/AddressRepository.java
@@ -8,14 +8,28 @@ import java.sql.ResultSet;
 public class AddressRepository extends AbstractRepository<Address, Integer>{
 
     //private static final String TABLE;
+    public static final String ADDRESS = "Address";
+    public static final String NR = "nr";
+    public static final String TYPE ="";
+    //public static final PostalCode Pcode;
     
     public AddressRepository() {
-        super("address", "");
+        super(ADDRESS, NR);
     }
 
     @Override
     public Address createObject(ResultSet resultSet) {
-        throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
+       
+     //To change body of generated methods, choose Tools | Templates.
+        try {
+            Address address = new Address();
+            address.setTypeAddress(resultSet.getString(ADDRESS));
+            address.setStreet(resultSet.getString(NR));
+                
+        } catch (Exception e) {
+        }
+        return null;
+    
     }
     
     
